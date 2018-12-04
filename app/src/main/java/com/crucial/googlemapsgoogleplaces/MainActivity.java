e417a9e308f3d3ab9bdf79b7368af043f02063f7
crucialjun <nich.otieno@gmail.com>
2018-12-04 15:53:01 +0000

    Play services check

--- app/src/main/java/com/crucial/googlemapsgoogleplaces/MainActivity.java
+++ app/src/main/java/com/crucial/googlemapsgoogleplaces/MainActivity.java
@@ -1,45 +1,66 @@
 package com.crucial.googlemapsgoogleplaces;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
-    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
-    private static final int LOCATION_PERMISSION_CODE = 1234;
+public class MainActivity extends AppCompatActivity{
+    private static final String TAG ="MainActivity";
 
-    private Boolean mLocationPermissionsGranted = false;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    private void getLocationPermission(){String[] permissions =
-            {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
-
-    if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED){
-        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED){
-            mLocationPermissionsGranted = true;
-
-        }else{
-            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_CODE);
+        if(isServicesOk()){
+            init();
         }
     }
+
+
+
+    private void init(){
+        Button btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MapActivity.class);
+            }
+        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    public boolean isServicesOk(){
+        Log.d(TAG,"isServiceOK: Checking google services version");
+
+        int available = GoogleApiAvailability.getInstance()
+                .isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //Services are fine and user can make map request
+            Log.d(TAG,"isServiceOk: Google Play Service is working");
+            return true;
+        } else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //Error Occured and its resolvable
+            Log.d(TAG,"isServiceOk: Google Play Service is working");
+            Dialog dialog = GoogleApiAvailability.getInstance()
+                    .getErrorDialog(MainActivity.this,available,ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else {
+            Toast.makeText(this, "You cant make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
+
+
+
 }
--- app/src/main/java/com/crucial/googlemapsgoogleplaces/MapActivity.java
+++ app/src/main/java/com/crucial/googlemapsgoogleplaces/MapActivity.java
@@ -1,14 +1,79 @@
 package com.crucial.googlemapsgoogleplaces;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+
     private static final String TAG ="MapActivity";
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_CODE = 1234;
+
+    private boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+
+    private void initMap(){
+        SupportMapFragment supportMapFragment =(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        supportMapFragment.getMapAsync(MapActivity.this);
+    }
+
+    private void getLocationPermission(){String[] permissions =
+            {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)
+                    == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+
+            }else{
+                ActivityCompat.requestPermissions(this,permissions, LOCATION_PERMISSION_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionsGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_CODE:{
+                for (int i = 0; i < grantResults.length; i++) {
+                    if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                        mLocationPermissionsGranted = false;
+                        return;
+                    }
+                }
+
+                mLocationPermissionsGranted = true;
+
+            }
+        }
     }
 }
