35580597b9daaff194f2dba9d3ad54af835ed5e5
crucialjun <nich.otieno@gmail.com>
2018-12-06 16:48:24 +0000

    Search Location

--- app/src/main/java/com/crucial/googlemapsgoogleplaces/MapActivity.java
+++ app/src/main/java/com/crucial/googlemapsgoogleplaces/MapActivity.java
@@ -2,6 +2,8 @@ package com.crucial.googlemapsgoogleplaces;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -9,6 +11,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -21,6 +27,9 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -39,19 +48,14 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                     ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.
                     checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                     != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 ActivityCompat.requestPermissions(MapActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_CODE);
 
             }
 
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+            init();
 
         }
 
@@ -66,6 +70,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private static final int LOCATION_PERMISSION_CODE = 1234;
     private static final float DEFAULT_ZOOM = 15f;
 
+    private EditText mSearchtext;
 
     private boolean mLocationPermissionsGranted = false;
     private GoogleMap mMap;
@@ -100,7 +105,46 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         mapFragment.getMapAsync(this);
 
+        mSearchtext = findViewById(R.id.input_search);
         getLocationPermission();
+    }
+
+    private void init(){
+        Log.d(TAG,"init: initializing");
+
+        mSearchtext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEARCH
+                        || actionId == EditorInfo.IME_ACTION_DONE
+                        || event.getAction() == KeyEvent.ACTION_DOWN
+                        || event.getAction() == KeyEvent.KEYCODE_ENTER){
+                    
+                    geoLocate();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void geoLocate() {
+        Log.d(TAG,"getDeviceLocation: getting the devices current location");
+
+        String searchString = mSearchtext.getText().toString();
+
+        Geocoder geocoder = new Geocoder(MapActivity.this);
+        List<Address> list = new ArrayList<>();
+        try {
+            list = geocoder.getFromLocationName(searchString,1);
+        }catch (IOException e){
+            Log.e(TAG,"geoLocate: IOException: "+ e.getMessage());
+        }
+
+        if(list.size() > 0){
+            Address address= list.get(0);
+
+            Log.d(TAG,"geoLocate: found a location: " + address.toString());
+        }
     }
 
     private void getDeviceLocation(){
